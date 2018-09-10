645fc0789aa43b9373c84e603ca3da1f7fff51d2
isaacsbestfriend <43147374+isaacsbestfriend@users.noreply.github.com>
2018-09-10 13:19:29 -0400

    Chris's Change 2018-09-10
    Added name

--- Java_Project/JavaProject.java
+++ Java_Project/JavaProject.java
@@ -2,5 +2,7 @@ public class JavaProject{
   public static void main(String[] args){
     System.out.println("Hello, world!");
     System.out.println("Esta Sam!");
+    System.out.println("Chris was here");
+    
   }
 }
