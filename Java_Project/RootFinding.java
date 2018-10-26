f5bbddcfd22cef8f35d7c8ffe1f140dc762ec832
Samantha Howse <shows058@uottawa.ca>
2018-10-26 11:34:31 -0230

    Oops mistakes fixed

--- Java_Project/RootFinding.java
+++ Java_Project/RootFinding.java
@@ -7,16 +7,16 @@ public class RootFinding{
     Scanner myScan = new Scanner(System.in);
     //currently assuming no negative powers in the equilibrium equation
     System.out.println("Enter the highest power of x within the equilibrium data.");
-    int j = myScan.nextInt;
+    int j = myScan.nextInt();
     while(j<0){
       System.out.println("That is not a valid power, please reenter a positive integer or exit the program.");
-      j = myScan.nextInt;
+      j = myScan.nextInt();
     }
     double[] coefficients = new double[j];
     System.out.println("Enter the coefficients of the equilibrium equation in increasing order.");
     for(int i = 0;i<j;i++){
       System.out.println("Enter the coefficient of x^"+i+".");
-      coefficients[i] = myScan.nextDouble;
+      coefficients[i] = myScan.nextDouble();
     }
     return coefficients;
   } 
@@ -26,4 +26,6 @@ public class RootFinding{
     for(int i = 0;i<coefficients.length;i++){
       y = y+Math.pow(x,i)*coefficients[i];
     }
+  return y;
+  }
 }
