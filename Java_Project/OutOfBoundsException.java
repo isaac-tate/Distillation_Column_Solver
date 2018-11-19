457cd1403e0eb5dd85f6119b34b5c178207050d7
Kaitlin Wagner <kwagn043@uottawa.ca>
2018-11-19 12:15:00 -0500

    More Exception Handling

--- Java_Project/OutOfBoundsException.java
+++ Java_Project/OutOfBoundsException.java
@@ -1,3 +1,20 @@
 public class OutOfBoundsException extends Exception {
   
+  private boolean outOfBounds;
+  
+  public OutOfBoundsException (boolean outOfBounds) { // constructor
+    super();
+    this.outOfBounds = outOfBounds;
+  }
+  
+  public OutOfBoundsException(OutOfBoundsException copy) {
+    super();
+    this.outOfBounds = copy.outOfBounds;
+  }
+  
+  public boolean getBounds() { // getter
+    return this.outOfBounds;
+  }
 }
+
+//NoRootsException
