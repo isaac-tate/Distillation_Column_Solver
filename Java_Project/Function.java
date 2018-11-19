3f0f8f10fad078cb09f63bd10d9d86ecad7ca40c
Samantha Howse <shows058@uottawa.ca>
2018-11-19 13:53:24 -0500

    Just like a new comment

--- Java_Project/Function.java
+++ Java_Project/Function.java
@@ -1,4 +1,4 @@
-public interface Function{//a generalized function parent class with children that are different functions
+public interface Function{//a generalized function interface that holds a setX method used for different functions
   
  public double setX(double x);//all function children contain a method of setX that returns a double
     
